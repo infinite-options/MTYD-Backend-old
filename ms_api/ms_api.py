e39d5fcd9695fe5b8530db7bc5e14d4de57f7956
Welkin <cchuang4@ucsc.edu>
2020-10-21 16:12:07 -0700

    updated ms_api file with signup

--- ms_api/ms_api.py
+++ ms_api/ms_api.py
@@ -3158,5 +3158,5 @@ api.add_resource(access_refresh_update, '/api/v2/access_refresh_update')
 # Make sure port number is unused (i.e. don't use numbers 0-1023)
 # lambda function at: https://ht56vci4v9.execute-api.us-west-1.amazonaws.com/dev
 if __name__ == '__main__':
-    #app.run(host='127.0.0.1', port=2000)
-    app.run(host='0.0.0.0', port=2000)
+    app.run(host='127.0.0.1', port=2000)
+    #app.run(host='0.0.0.0', port=2000)
