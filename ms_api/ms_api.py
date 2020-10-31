561c3834b6e680434adab12de7a4c4fc22ca0c39
Welkin <cchuang4@ucsc.edu>
2020-10-31 14:05:45 -0700

    updated ms_api file with newest code

--- ms_api/ms_api.py
+++ ms_api/ms_api.py
@@ -3929,5 +3929,5 @@ api.add_resource(Send_Twilio_SMS, '/api/v2/Send_Twilio_SMS')
 # Make sure port number is unused (i.e. don't use numbers 0-1023)
 # lambda function at: https://ht56vci4v9.execute-api.us-west-1.amazonaws.com/dev
 if __name__ == '__main__':
-    #app.run(host='127.0.0.1', port=2000)
-    app.run(host='0.0.0.0', port=2000)
+    app.run(host='127.0.0.1', port=2000)
+    #app.run(host='0.0.0.0', port=2000)
